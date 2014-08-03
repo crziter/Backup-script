32bb1b64611e96804f6ad3e9994bf8dcfd09d2cc
crziter <crziter@gmail.com>
2014-08-04 01:24:33 +0700

    Add usage to add-repo.sh, rm-repo.sh and mark as finish in do-backup.sh

--- add-repo.sh
+++ add-repo.sh
@@ -3,4 +3,14 @@
 # $1: repo name
 # $2: repo location
 
-echo "$1|$2" >> repos.txt
+function print_usage {
+	printf "Usage: $0 repo_name /path/to/repo\n"
+}
+
+if [ "$#" -ne 2 ]
+then
+	print_usage
+else
+	echo "$1|$2" >> repos.txt
+	echo "Repo $1 has been added to library"
+fi
--- rm-repo.sh
+++ rm-repo.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 # $1: repo name
 
-found=0
-while read line
-do
-	repo_name=$(echo $line | cut -d'|' -f1)
-	if [ "$repo_name" == "$1" ]
-	then
-		found=1
-	else
-		echo $line >> repos.tmp
-	fi
-done < repos.txt
+function print_usage {
+	printf "Usage: $0 repo_name\n"
+}
 
-if [ $found -eq 1 ] # Found repo
-then
-	rm repos.txt
-
-	if [ -f "repos.tmp" ]
-	then
-		mv repos.tmp repos.txt
-	else
-		touch repos.txt
-	fi
-
-	echo "Repo $1 is removed"
+if [ "$#" -ne 1 ]
+then 
+	print_usage
 else
-	rm repos.tmp
-	echo "Repo $1 not found"
+	found=0
+	while read line
+	do
+		repo_name=$(echo $line | cut -d'|' -f1)
+		if [ "$repo_name" == "$1" ]
+		then
+			found=1
+		else
+			echo $line >> repos.tmp
+		fi
+	done < repos.txt
+
+	if [ $found -eq 1 ] # Found repo
+	then
+		rm repos.txt
+
+		if [ -f "repos.tmp" ]
+		then
+			mv repos.tmp repos.txt
+		else
+			touch repos.txt
+		fi
+
+		echo "Repo $1 is removed"
+	else
+		rm repos.tmp
+		echo "Repo $1 not found"
+	fi
 fi
